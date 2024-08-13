47ea5e5febb7d045bc3ef68cf814edc62fd449dd
Prokop Ferdan <prokes1@gmail.com>
2024-08-13 09:41:03 +0200

    added /procesFormVersionTwo mapping to uadd uppercase studentName to model

--- src/main/java/com/prokys/demoThymeleaf/HelloWorldController.java
+++ src/main/java/com/prokys/demoThymeleaf/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.prokys.demoThymeleaf;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,5 +19,25 @@ public class HelloWorldController {
     @GetMapping("/processForm")
     public String processForm(){
         return "helloworld";
+    }
+
+    // need a controller to read form data and add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model){
+
+        // read the request parameter from HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! "+theName;
+
+        //add message to model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+
     }
 }
