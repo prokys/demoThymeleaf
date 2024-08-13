3e4a7d4342cc4d827227bda4d495dd5f2bf96ab6
Prokop Ferdan <prokes1@gmail.com>
2024-08-13 10:50:57 +0200

    added /processStudentForm mapping and thymeleaf html templates for student-form and student-confirmation

--- src/main/java/com/prokys/demoThymeleaf/controller/StudentController.java
+++ src/main/java/com/prokys/demoThymeleaf/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.prokys.demoThymeleaf.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -17,4 +19,13 @@ public class StudentController {
 
         return "student-form";
     }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent){
+        // log the input data
+        System.out.println("theStudent "+ theStudent.getFirstName()+ " "+theStudent.getLastName());
+
+        return "student-confirmation";
+    }
+
 }
