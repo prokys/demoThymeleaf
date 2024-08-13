0668145bc48cc72f204d7d3a3cf237d2152b91ec
Prokop Ferdan <prokes1@gmail.com>
2024-08-13 18:03:45 +0200

    added customer-form template, updated CustomerController with /processForm endpoint

--- src/main/java/com/prokys/demoThymeleaf/controller/CustomerController.java
+++ src/main/java/com/prokys/demoThymeleaf/controller/CustomerController.java
@@ -1,9 +1,13 @@
 package com.prokys.demoThymeleaf.controller;
 
 import com.prokys.demoThymeleaf.model.Customer;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
@@ -14,5 +18,15 @@ public class CustomerController {
         theModel.addAttribute("customer", new Customer());
 
         return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult){
+
+        if (theBindingResult.hasErrors()){
+            return "customer-form";
+        }else {
+            return "customer-confirmation";
+        }
     }
 }
