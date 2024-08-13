7a0c634ebf0b829f2bde6020bacb713ee0600343
Prokop Ferdan <prokes1@gmail.com>
2024-08-13 14:18:47 +0200

    updated Student class with country, updated StudentController to add countries into model, added countries to app.properties and updated html files

--- src/main/java/com/prokys/demoThymeleaf/controller/StudentController.java
+++ src/main/java/com/prokys/demoThymeleaf/controller/StudentController.java
@@ -1,14 +1,20 @@
 package com.prokys.demoThymeleaf.controller;
 
 import com.prokys.demoThymeleaf.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+    @Value("${countries}")
+    private List<String> countries;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
         //create a new student object
@@ -17,6 +23,8 @@ public class StudentController {
         //add student into model
         theModel.addAttribute("student", theStudent);
 
+        // add the list of countries to the model
+        theModel.addAttribute("countries", countries);
         return "student-form";
     }
 
