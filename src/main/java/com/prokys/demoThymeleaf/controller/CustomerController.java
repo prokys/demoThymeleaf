4ee7136348de5b95dae460adea7ddfac126d7121
Prokop Ferdan <prokes1@gmail.com>
2024-08-14 09:23:10 +0200

    added @InitBinder to trim whitespaces from strings

--- src/main/java/com/prokys/demoThymeleaf/controller/CustomerController.java
+++ src/main/java/com/prokys/demoThymeleaf/controller/CustomerController.java
@@ -2,15 +2,29 @@ package com.prokys.demoThymeleaf.controller;
 
 import com.prokys.demoThymeleaf.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // add initbinder to trim input strings
+
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel){
